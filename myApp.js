0d7f251467c86588aba23d2ca1c651dd0159c7f3
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 03:09:32 +0100

    Challenge 9 Done

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 
 
-// Challenge 9
+// Challenge 9 - Get Route Parameter Input from Client
 app.get('/:word/echo', (req, res) => {
     res.json({
         echo: req.params.word
