02e613962ad5c9a8b043066892de6e53929c3b75
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 02:38:06 +0100

    Challenge 6 Done

--- myApp.js
+++ myApp.js
@@ -1,39 +1,21 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
+// Challenge 6
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({
+            "message": "HELLO JSON"
+        })
+    } else {
+        res.json({
+            "message": "Hello json"
+        })
+    }
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
