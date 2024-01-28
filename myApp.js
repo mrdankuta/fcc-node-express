a784d052c284eec65ab0590ecdbc49c29bae0219
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 02:33:12 +0100

    Challenge 2 Done

--- myApp.js
+++ myApp.js
@@ -1,9 +1,6 @@
 let express = require('express');
 let app = express();
 
-// Challenge 1
-// console.log('Hello World');
-
 //Challenge 2
 app.get('/', function (req, res) {
     res.send('Hello Express');
