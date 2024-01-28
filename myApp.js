2e2b3dc76286fa9207d610144f3adb0dbfe307eb
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 03:40:54 +0100

    Challenge 10 Done

--- myApp.js
+++ myApp.js
@@ -4,24 +4,26 @@ let app = express();
 
 
 // Challenge 10 - Get Query Parameter Input from Client
-// app.get('/name', (req, res) => {
-//     let fullname = req.query.first + ' ' + req.query.last;
-//     res.json({
-//         name: fullname
-//     });
-// })
+app.get('/name', (req, res) => {
+    let firstname = req.query.first;
+    let lastname = req.query.last;
+    res.json({
+        name: firstname + ' ' + lastname,
+    });
+})
 
 // V2
-app.route('/name')
-    .get((req, res) => {
-        let fullname = req.query.first + ' ' + req.query.last;
-        res.json({
-            name: fullname
-        });
-    })
-    .post((req, res) => {
-        //...
-    })
+// app.route('/name')
+//     .get((req, res) => {
+//         let firstname = req.query.first;
+//         let lastname = req.query.last;
+//         res.json({
+//             name: firstname + ' ' + lastname
+//         });
+//     })
+//     .post((req, res) => {
+//         //...
+//     })
 
 
 
