835ce95c9e646ccaf2ac2f619bfb8353dc6f0686
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 03:55:44 +0100

    Challenge 11 Done

--- myApp.js
+++ myApp.js
@@ -1,30 +1,10 @@
 require('dotenv').config();
+let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
 
 
-// Challenge 10 - Get Query Parameter Input from Client
-app.get('/name', (req, res) => {
-    let firstname = req.query.first;
-    let lastname = req.query.last;
-    res.json({
-        name: firstname + ' ' + lastname,
-    });
-})
-
-// V2
-// app.route('/name')
-//     .get((req, res) => {
-//         let firstname = req.query.first;
-//         let lastname = req.query.last;
-//         res.json({
-//             name: firstname + ' ' + lastname
-//         });
-//     })
-//     .post((req, res) => {
-//         //...
-//     })
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
