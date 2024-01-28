0b7955e8f7c9242b26d7bbb496675bf8ba58b7a0
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 02:39:46 +0100

    Challenge 7 Done

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
 
+// Challenge 7
+app.use((req, res, next) => {
+    console.log(req.method, req.path, "-", req.ip);
+    next();
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
