9e24132d4e31d2ebe69961928aa4fa8d29645115
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 04:16:48 +0100

    Challenge 12 Done

--- myApp.js
+++ myApp.js
@@ -3,10 +3,23 @@ let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
 
-// Challenge 11
+// Challenge 12
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('/public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.post('/name', (req, res) => {
+    let firstname = req.body.first;
+    let lastname = req.body.last;
+    res.json({
+        name: firstname + ' ' + lastname
+    })
+})
 
 
 
