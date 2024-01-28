85ac8e894e9693c6b1bbad15301316352afd4592
Dan Kuta <daniel@rarefyconsulting.com>
2024-01-28 02:49:44 +0100

    Challenge 8 Done

--- myApp.js
+++ myApp.js
@@ -1,39 +1,21 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
 
+// Challenge 8
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({
+        time: req.time
+    })
+})
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
